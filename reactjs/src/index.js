8afabc8930374d8dccd17d927accd305781c0a16
greciodiogo <fonebahia8@gmail.com>
2022-03-16 08:37:39 +0100

    The global styles for this aplication were defined

---
+++ reactjs/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './app';
+import {GlobalStyles} from './global-styles'
+
+render(<>
+    <GlobalStyles/>
+    <App/>
+    </>,
+    document.getElementById('root'));
+
