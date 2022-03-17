3db36ecedbb8ce671d8924a2b048aff93ad08a1c
greciodiogo <fonebahia8@gmail.com>
2022-03-17 09:49:13 +0100

    I encapsulated and assigned the functions and components. They are read to be called in others pages

---
+++ reactjs/src/components/opt-form/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Container, Input, Text, Button, Break} from './styles/opt-form'
+
+export default function OptForm({children, ...restProps}) {
+  return (
+    <Container {...restProps}>{children}</Container>
+  )
+}
+
+OptForm.Input = function OptFormInput({ ...restProps}) {
+    return <Input {...restProps} />
+}
+OptForm.Break = function OptFormBreak({ ...restProps}) {
+    return <Break {...restProps} />
+}
+OptForm.Button = function OptFormButton({ children, ...restProps}) {
+    return ( 
+    <Button {...restProps}>
+        {children} <img src="/images/icons/chevron-right.png" alt='Try it now'/>
+    </Button>
+    )
+}
+OptForm.Text = function OptFormText({ children, ...restProps}) {
+    return <Text {...restProps}>{children}</Text>
+}
