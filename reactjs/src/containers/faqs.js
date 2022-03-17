036e0f3c11d078156874d81a744c50082c64aad3
greciodiogo <fonebahia8@gmail.com>
2022-03-17 11:01:19 +0100

    I took the word default from thefunction

--- reactjs/src/containers/faqs.js
+++ reactjs/src/containers/faqs.js
@@ -3,7 +3,7 @@ import {Accordion} from '../components'
 import OptForm from '../components/opt-form'
 import faqsData from '../fixtures/faqs.json'
 
-export default function FaqContainer() {
+export function FaqContainer() {
   return (
     <Accordion>
       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
