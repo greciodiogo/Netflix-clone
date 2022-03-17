0897ff385d6df66dd76b810c84799918eaf16d3e
greciodiogo <fonebahia8@gmail.com>
2022-03-17 10:01:19 +0100

    I adjusted the margin to auto and the width

--- reactjs/src/components/accordion/styles/accordion.js
+++ reactjs/src/components/accordion/styles/accordion.js
@@ -27,6 +27,8 @@ export const Item = styled.div`
     color: white;
     margin-bottom:10px;
     max-width: 670px;
+    margin:auto;
+    width: 100%;
 
     &:first-of-type {
         margin-top: 3em;
