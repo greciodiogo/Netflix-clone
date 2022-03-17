173fa4aacbc44ae7e1490ca085946afba8bec2c1
greciodiogo <fonebahia8@gmail.com>
2022-03-17 07:54:22 +0100

    The accordion was additioned to app page

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
+import FaqContainer from './containers/faqs';
 
 export default function App () {  
   return (
     <>
       <JumbotronContainer/>
+      <FaqContainer/>   
       <FooterContainer/>
     </>
   );
