4514388c01b12c32498418f2953c5c69105ef9e8
greciodiogo <fonebahia8@gmail.com>
2022-03-17 10:56:53 +0100

    I add the Routes

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import FaqContainer from './containers/faqs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import *as ROUTES from './constants/routes'
+import { Browse, Home, Signin, Signup } from './pages';
 
 export default function App () {  
   return (
-    <>
-      <JumbotronContainer/>
-      <FaqContainer/>   
-      <FooterContainer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home/>}/>
+        <Route exact path={ROUTES.BROWSE} element={<Browse/>}/>
+        <Route exact path={ROUTES.SIGN_IN} element={<Signin/>}/>
+        <Route exact path={ROUTES.SIGN_UP} element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
