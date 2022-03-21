31c19f96cdb59ad0e18e58df7f6881a65508005d
greciodiogo <fonebahia8@gmail.com>
2022-03-21 13:48:52 +0100

    I incremented the FirebaseContext

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './app';
+import { FirebaseContext } from './context/firebase';
 import {GlobalStyles} from './global-styles'
+//import { firebase } from './lib/firebase.prod';
 
-render(<>
-    <GlobalStyles/>
-    <App/>
+render(
+    <>
+    <FirebaseContext.Provider value={{/*firebase*/}}>
+        <GlobalStyles/>
+        <App/>
+    </FirebaseContext.Provider>
     </>,
     document.getElementById('root'));
 
