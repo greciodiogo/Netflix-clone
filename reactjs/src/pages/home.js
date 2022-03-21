5d66898955425814152abd6e3829140c014584d6
greciodiogo <fonebahia8@gmail.com>
2022-03-21 14:24:38 +0100

    I add the HeaderContainer in top of page. There is a form component withh text, input and button

--- reactjs/src/pages/home.js
+++ reactjs/src/pages/home.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import {HeaderContainer} from '../containers/header'
 import {FaqContainer} from '../containers/faqs'
 import {FooterContainer} from '../containers/footer'
 import {JumbotronContainer} from '../containers/jumbotron'
+import { Feature, OptForm } from '../components'
 
 export default function Home() {
   return (
     <>
+        <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV programmes and more</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time. </Feature.SubTitle>
+          <OptForm>
+              <OptForm.Input placeholder="Email address" /> 
+              <OptForm.Button>Try it now</OptForm.Button> 
+              <OptForm.Break/> 
+              <OptForm.Text>Read to watch? Enter your email to create or restart 
+                your membership</OptForm.Text> 
+          </OptForm>
+        </Feature>
+        </HeaderContainer>
         <JumbotronContainer/>
         <FaqContainer/>   
         <FooterContainer/>
